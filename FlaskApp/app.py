be0ef15103c8989d0d8108539c546609b5dae106
HebaShaban <128882939+HebaShaban@users.noreply.github.com>
2023-08-08 16:01:45 +0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = os.environ['DB_USER']
-app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['DB_PASSWORD']
-app.config['MYSQL_DATABASE_DB'] = os.environ['DB_NAME']
-app.config['MYSQL_DATABASE_HOST'] = os.environ['DB_HOST']
-app.config['MYSQL_DATABASE_PORT'] = int(os.environ['DB_PORT'])
+app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_NAME']
+app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_HOST']
+app.config['MYSQL_DATABASE_PORT'] = int(os.environ['MYSQL_PORT'])
 
 
 mysql.init_app(app)
